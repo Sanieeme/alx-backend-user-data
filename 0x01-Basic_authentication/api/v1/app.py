d2517ad15ca40ee8b6d1edd22b3ddf58198dcfae
sanieeme <shoneesani@gmail.com>
2024-08-08 08:28:44 +0000

    auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -32,6 +32,7 @@ def forbidden(error) -> str:
     """ forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
