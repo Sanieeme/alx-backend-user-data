519ae886bfdac175580cd2f9d2418d2204fd78a9
sanieeme <shoneesani@gmail.com>
2024-08-09 05:39:41 +0000

    pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -25,7 +25,7 @@ class Auth:
         """ public method"""
         if request in None:
             return None
-        return  request.headers.get('Authorization')
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """public method"""
