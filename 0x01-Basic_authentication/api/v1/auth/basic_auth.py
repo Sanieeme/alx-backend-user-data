e9bd79970de0e96c9a88a25746dc085492510072
sanieeme <shoneesani@gmail.com>
2024-08-09 07:40:28 +0000

    all

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """import modules"""
 from api.v1.auth.auth import Auth
+from models.user import User
 import base64
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -38,3 +40,39 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        if not isinstance(user_email, str):
+            return None
+        if not isinstance(user_pwd, str):
+            return None
+        user = User.search(user_email)
+        if user is None:
+            return None
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """method """
+        auth_header = self.authorization_header(request)
+        if not auth_header:
+            return None
+
+        base64_auth_header = self.extract_base64_authorization_header(auth_header)
+        if not base64_auth_header:
+            return None
+
+        decoded_auth_header = self.decode_base64_authorization_header(base64_auth_header)
+        if not decoded_auth_header:
+            return None
+
+        credentials = self.extract_user_credentials(decoded_auth_header)
+        if not credentials:
+            return None
+
+        user_email, user_pwd = credentials
+        return self.user_object_from_credentials(user_email, user_pwd)
