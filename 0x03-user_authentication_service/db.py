a6442610262ec8c4c2841ff804929c76310ca50a
sanieeme <shoneesani@gmail.com>
2024-08-16 10:19:10 +0000

    find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User
-from user import Base
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import User, Base
 
 
 class DB:
@@ -31,9 +32,26 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ method, which has two required string arguments
+        """method, which has two required string arguments
+        Args:
+            email (str): email
+            hashed_password (str): hashed password
+        Returns:
+            User: user object
         """
         new_user = User(email=email, hashed_password=hashed_password)
+
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """method takes in arbitrary keyword arguments
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found with the provided criteria.")
+        except Exception as e:
+            raise InvalidRequestError(f"Invalid query: {str(e)}")
